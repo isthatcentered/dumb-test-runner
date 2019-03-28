a53a23d94304542b3902a9754d9eafa288448829
Edouard Penin <e.peninb@gmail.com>
2019-03-28 10:15:52 +0100

    basic tests

---
+++ src/proxy.spec.ts
@@ -0,0 +1,25 @@
+import "jest-then"
+
+
+
+// can call anything
+// can get anything
+// returns name on tostring
+// returns origininame.property on get[key].tostring
+// if provided value !== primitive, return procy
+// if provided value = primitive, fetch target item
+
+
+describe( `toString()`, () => {
+	test( `Returns name of object if provided`, () => {
+	
+	} )
+	
+	test( `Returns nameOfObject.property when getting a sub property`, () => {
+	
+	} )
+	
+	test( `Returns fallback value if available (and not object?)`, () => {
+	
+	} )
+} )
