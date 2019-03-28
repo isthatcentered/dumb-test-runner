4f40d49a7c43969f02e4b9da4d01c252cbe86fd3
Edouard Penin <e.peninb@gmail.com>
2019-03-28 10:06:30 +0100

    todos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 console.log( "Victory" )
+// can call anything
+// can get anything
+// returns name on tostring
+// returns origininame.property on get[key].tostring
+// if provided value !== primitive, return procy
+// if provided value = primitive, fetch target item
+
