21a1c01d3644c97657326b43bc2e822ff1a94809
Edouard Penin <e.peninb@gmail.com>
2019-03-28 10:58:04 +0100

    stub.property.toString() returns 'stub.propery'

--- src/proxy.spec.ts
+++ src/proxy.spec.ts
@@ -1,45 +1,59 @@
 import "jest-then"
 
 
-
-// can call anything
+// can be identified
 // can get anything
+// can call anything
 // returns name on tostring
 // returns origininame.property on get[key].tostring
 // if provided value !== primitive, return procy
 // if provided value = primitive, fetch target item
 
-function stub<T>( identifier: string )
+function stub<T>( identifier: string ): T
 {
 	return new Proxy( function () {
 	
 	}, {
 		get( target, key, receiver ): any
 		{
-			console.log( "get:::", key, identifier.toString() )
+			console.log( "get:::", key)
 			
 			if ( key === "toString" )
 				return () => identifier.toString()
 			
-			return identifier
+			return stub( `${identifier}.${key.toString()}` )
 		},
 		apply()
 		{
 		
 		},
-	} )
+	} ) as any as T // @todo: fix that / can it be ?
 }
 
 
 describe( `toString()`, () => {
+	interface randomObject
+	{
+		nested: {
+			nestedLevel2: {
+				property: string
+			}
+			property: number
+		}
+		property: number
+	}
+	
 	test( `Returns name of object if provided`, () => {
-		const identifier = "IDENTIFIER"
+		const identifier = "identifier"
 		
-		expect( stub( identifier ).toString() ).toEqual( identifier )
+		expect( stub<randomObject>( identifier ).toString() ).toEqual( identifier )
 	} )
 	
 	test( `Returns nameOfObject.property when getting a sub property`, () => {
-	
+		const identifier = "identifier",
+		      _stub      = stub<randomObject>( identifier )
+		
+		expect( _stub.nested.nestedLevel2.property.toString() ).toEqual( `${identifier}.nested.nestedLevel2.property` )
 	} )
 	
 	test( `Returns fallback value if available (and not object?)`, () => {
