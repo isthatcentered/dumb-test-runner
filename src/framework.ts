f08defab4e65daad37dc068dab6dc18c4f8ecec6
Edouard Penin <e.peninb@gmail.com>
2019-03-29 07:10:49 +0100

    test()

--- src/framework.ts
+++ src/framework.ts
@@ -3,11 +3,44 @@ import { Expectation } from "./Expectation"
 
 
 
+class Test
+{
+	private __config: { title: string; block: () => void }
+	
+	
+	constructor( config: { title: string, block: () => void } )
+	{
+		this.__config = config
+	}
+	
+	
+	run()
+	{
+		try {
+			this.__config.block()
+			console.log( " 👍 ", this.__config.title )
+			// ✓ ✗
+		} catch ( e ) {
+			console.log( " 💩 ", this.__config.title )
+			console.log( e )
+		}
+	}
+}
+
 const verify = <T>( result: T ) => {
 	return Expectation.for( result )
 }
 
+const test = ( title: string, block: () => void ) =>
+	new Test( { title, block } )
 
-verify( 5 ).is( 5 )
 
-verify( () => verify( 5 ).is( 4 ) ).throws()
+test( "Logs a success", () => {
+	verify( 5 ).returns( 5 )
+} ).run()
+
+test( "Logs an error", () => {
+	verify( 5 ).returns( 4 )
+} ).run()
+
+// @todo: syntax 5.returns(2)
